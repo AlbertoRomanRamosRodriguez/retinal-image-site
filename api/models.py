c43d09741ac7daed4e4cbaeb0c885b72fcf820ae
Alberto R Ramos <rr.albertoroman@gmail.com>
2023-03-04 15:27:59 -0500

    fixing image field

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class OFundus(models.Model):
     class_name = models.CharField(max_length=60,null=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='Images/', default='Images/None/No0img.jpg')
 
     def __str__(self) -> str:
         return f'{self.class_name}'
--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 from . import models
 
 class OFSerializer(serializers.HyperlinkedModelSerializer):
+
+    image = serializers.ImageField(max_length=None, use_url=True)
+
     class Meta:
         model = models.OFundus
         fields = ('id', 'image', 'class_name')
